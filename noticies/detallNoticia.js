4e174ef4cb4d836c55068ec51a4191d7708bafde
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2025-05-30 19:12:21 +0000

    CanoJoel_Glamour

--- noticies/detallNoticia.js
+++ noticies/detallNoticia.js
@@ -7,7 +7,7 @@ const noticia = bd[id];
 if (noticia) {
   main.innerHTML = `
     <div class="contenidoArticulo">
-      <img class="imgDetalle" src="../${noticia.img}" alt="${noticia.tituloNoticia}">
+      <img class="imgDetalle" src="${noticia.img}" alt="${noticia.tituloNoticia}">
       <div class="textoArticulo">
         <h1 class="titolArticle">${noticia.tituloNoticia}</h1>
         ${noticia.introduccion}
